cba6f2b80f117dfadc6ee70fc81c720205e2edb9
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-15 23:10:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,10 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
-    return array.find(cheese_types)
-  else
-    return nil
-  end
+  return array.find(cheese_types)
+  # if array.include?(cheese_types)
+  #   
+  # else
+  #   return nil
+  # end
 end
