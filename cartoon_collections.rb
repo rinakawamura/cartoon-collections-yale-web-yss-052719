59efe456fdd663785ac5006408457387b75566a6
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-15 22:51:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,12 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?(cheese_types)
+    return array[array.index(cheese_types)]
+  else
+    return nil
+  end
 end
-
-summon_captain_planet(["hello", "bye"])
