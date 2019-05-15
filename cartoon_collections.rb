2de8fde094f7f021ff019164c656bba4e574816e
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-15 23:12:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |arg|
     cheese_types.include?(arg)
